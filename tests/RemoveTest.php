f1e7b254b8917567a51948605b79fcea0bc7e38b
livingsneaky-art <ryanllamido@gmail.com>
2023-05-20 18:52:59 +0800

    Create RemoveTest.php

---
+++ tests/RemoveTest.php
@@ -0,0 +1,65 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class RemoveTest extends TestCase
+{
+    protected $db;
+
+    protected function setUp(): void
+    {
+        // Create a database connection
+        $host = '127.0.0.1';
+        $username = 'your_username';
+        $password = 'your_password';
+        $database = 'ismis_db';
+        $port = 3306;
+
+        $this->db = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4", $username, $password);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    protected function tearDown(): void
+    {
+        // Close the database connection
+        $this->db = null;
+    }
+
+    public function test_admin_remove_instructor()
+    {
+        // Insert a new instructor into the 'users' table
+        $instructorName = 'John Doe';
+        $instructorId = 12345;
+
+        $query = "INSERT INTO users (name, id) VALUES (:name, :id)";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':name', $instructorName);
+        $statement->bindParam(':id', $instructorId);
+        $statement->execute();
+
+        // Check if the instructor was inserted successfully
+        $rowCount = $statement->rowCount();
+        $this->assertEquals(1, $rowCount, "Failed to insert a new instructor.");
+
+        // Remove the instructor from the 'users' table
+        $query = "DELETE FROM users WHERE id = :id";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':id', $instructorId);
+        $statement->execute();
+
+        // Check if the instructor was removed successfully
+        $rowCount = $statement->rowCount();
+        $this->assertEquals(1, $rowCount, "Failed to remove the instructor.");
+
+        // Verify that the instructor is no longer in the database
+        $query = "SELECT * FROM users WHERE id = :id";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':id', $instructorId);
+        $statement->execute();
+
+        $instructor = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $this->assertFalse($instructor);
+    }
+}
+
+?>
