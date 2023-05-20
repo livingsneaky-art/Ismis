a90d0611f6833180091b06c06ccb1e0760b940a5
livingsneaky-art <ryanllamido@gmail.com>
2023-05-20 18:24:42 +0800

    update authenticatTest

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    public function testSuccessfulLoginRedirectsToIndexStudent()
+    public function test_user_login()
     {
         $_POST['id_num'] = '20300001';
         $_POST['password'] = '123';
@@ -21,7 +21,7 @@ class AuthenticationTest extends TestCase
         $this->assertStringContainsString('Location: IndexStudent', $output);
     }
 
-    public function testFailedLoginRedirectsToIndexStudent()
+    public function test_user_login_failed()
     {
         $_POST['id_num'] = '20300001';
         $_POST['password'] = 'wrongpassword'; // Provide an incorrect password
@@ -38,7 +38,7 @@ class AuthenticationTest extends TestCase
         $this->assertRedirect('IndexStudent');
     }
 
-    public function testSuccessfulRegistrationRedirectsToIndexStudent()
+    public function test_user_register()
     {
         $_POST['id_num'] = '20300001';
         $_POST['password'] = '123';
@@ -58,7 +58,7 @@ class AuthenticationTest extends TestCase
         $this->assertRedirect('IndexStudent');
     }
 
-    public function testFailedRegistrationRedirectsToLoginPage()
+    public function test_user_register_failed()
     {
         $_POST['id_num'] = '20300001';
         $_POST['password'] = '123';
@@ -77,6 +77,69 @@ class AuthenticationTest extends TestCase
         // Assert that the page redirected to the login page
         $this->assertRedirect('login.php');
     }
+
+    public function test_login_can_access_home_student()
+    {
+        // Simulate a successful login with valid credentials
+        $_POST['id_num'] = '20300001';
+        $_POST['password'] = '123';
+
+        // Include the server script to make the necessary variables and functions available
+        require './includes/server.php';
+
+        // Start output buffering to capture the redirect
+        ob_start();
+        require 'login.php';
+        ob_end_clean();
+
+        // Assert that the page redirected to the "IndexStudent" page
+        $this->assertRedirect('IndexStudent');
+    }
+
+    public function test_login_can_access_home_faculty()
+    {
+        // Simulate a successful login with valid credentials
+        $_POST['id_num'] = '20300002';
+        $_POST['password'] = '123';
+
+        // Include the server script to make the necessary variables and functions available
+        require './includes/server.php';
+
+        // Start output buffering to capture the redirect
+        ob_start();
+        require 'login.php';
+        ob_end_clean();
+
+        // Assert that the page redirected to the "IndexStudent" page
+        $this->assertRedirect('IndexFaculty');
+    }
+
+
+    public function test_user_logout(){
+        // Simulate a successful login with valid credentials
+        $_POST['id_num'] = '20300001';
+        $_POST['password'] = '123';
+
+        // Include the server script to make the necessary variables and functions available
+        require './includes/server.php';
+
+        // Start output buffering to capture the redirect
+        ob_start();
+        require 'login.php';
+        ob_end_clean();
+
+        // Assert that the page redirected to the "IndexStudent" page
+        $this->assertRedirect('IndexStudent');
+
+        // Start output buffering to capture the redirect
+        ob_start();
+        require 'logout.php';
+        ob_end_clean();
+
+        // Assert that the page redirected to the "IndexStudent" page
+        $this->assertRedirect('login.php');
+    }
+    
 }
 
 ?>
